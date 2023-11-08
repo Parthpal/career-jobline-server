cf0244e4e0c93e6b1dcc8b000544e03380e7f32b
Parthpal <parthapradippal@gmail.com>
2023-11-08 21:34:27 +0530

    issue

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
 const express = require('express')
 const jwt = require('jsonwebtoken');
+var cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000;
 //to get cookies from client side we require cookie purser
 const cookieParser=require('cookie-parser')
-const app = express()
+// app.use(express.json());
+// const corsConfig = {
+// origin: '*',
+// credentials: true,
+// methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+// }
+// app.use(cors(corsConfig))
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-var cors = require('cors')
+
 
 //middleware: just connecting client and server
 app.use(cors({
-  origin:['http://localhost:5173'],
+  origin:[
+  // ['http://localhost:5173',
+  'https://ph-jobline-df019.web.app/',
+  'https://ph-jobline-df019.firebaseapp.com/'
+],
   credentials:true
 }))
-app.use(cookieParser())
-const verifyToken=async(req,res,next)=>{
-  //here we are receiving the token which is stored at client side cookies
-  const token=req.cookies?.token;
-  console.log('value of token in middleware,receiving from client side',token);
-  if(!token)
-  {
-    return res.status(401).send({message:'Not Authorized'})
-  }
-  jwt.verify(token,process.env.secret_code,(err,decoded)=>{
-    if(err){
-      console.log(err);
-      return res.status(401).send({message:'Not Authorized'})
-    }
-    console.log('found match! value in the token',decoded);
-    req.user=decoded;
-    next();
-  })
+// app.use(cookieParser())
+// const verifyToken=async(req,res,next)=>{
+//   //here we are receiving the token which is stored at client side cookies
+//   const token=req.cookies?.token;
+//   console.log('value of token in middleware,receiving from client side',token);
+//   if(!token)
+//   {
+//     return res.status(401).send({message:'Not Authorized'})
+//   }
+//   jwt.verify(token,process.env.secret_code,(err,decoded)=>{
+//     if(err){
+//       console.log(err);
+//       return res.status(401).send({message:'Not Authorized'})
+//     }
+//     console.log('found match! value in the token',decoded);
+//     req.user=decoded;
+//     next();
+//   })
   
-}
+// }
 app.use(express.json())
-const port = 3000 || process.env.PORT;
+
+
 //client sending request to server for data that is req
 //on that req server send response that is res
 
 
  
+//const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.kf7gnio.mongodb.net/?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.kf7gnio.mongodb.net/?retryWrites=true&w=majority`;
 console.log();
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,7 +69,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   // await client.connect();
     const database = client.db('jobline');
     const job_cat_collection = database.collection('job_category');
     const job_collection=database.collection('job')
@@ -63,24 +79,24 @@ async function run() {
         // token related api
     // For creation of token, creating a route in server called jwt 
 
-    app.post('/jwt',async(req,res)=>{
-      const user=req.body; // here,in server we are fetching client details like email send by client
-      console.log(user,'we are starting creation of token');
-      const token=jwt.sign(user,process.env.secret_code,{ expiresIn: '1h' })
-      console.log(user,'Token Created successfully',token);
-      res.cookie('token',token,{
-        httpOnly:true,
-        secure:false,
-        sameSite:'none'
-      })
-      .send({success:true});
-    })
+    // app.post('/jwt',async(req,res)=>{
+    //   const user=req.body; // here,in server we are fetching client details like email send by client
+    //   console.log(user,'we are starting creation of token');
+    //   const token=jwt.sign(user,process.env.secret_code,{ expiresIn: '1h' })
+    //   console.log(user,'Token Created successfully',token);
+    //   res.cookie('token',token,{
+    //     httpOnly:true,
+    //     secure:false,
+    //     sameSite:'none'
+    //   })
+    //   .send({success:true});
+    // })
 
-    app.post('/logOut',async(req,res)=>{
-      const user=req.body;
-      console.log(user,'logged out');
-      res.clearCookie('token',{maxAge:0}).send({success:true})
-    })
+    // app.post('/logOut',async(req,res)=>{
+    //   const user=req.body;
+    //   console.log(user,'logged out');
+    //   res.clearCookie('token',{maxAge:0}).send({success:true})
+    // })
 
 
     //update form
